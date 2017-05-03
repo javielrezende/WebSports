097e350ce450d5824c3cfd59de6451728df5ac65
William Kolosque <williammaich@hotmail.com>
2017-05-03 11:35:30 -0300

    criado a tabela cidade e alterado o endereco;

--- app/models/Cidade.java
+++ app/models/Cidade.java
@@ -11,16 +11,12 @@ import javax.persistence.ManyToOne;
  * Created by William on 03/05/2017.
  */
 @Entity
-public class Endereco extends Model{
+public class Cidade extends Model{
     @Id
     public Long id;
-    public String rua;
-    public int numero;
-    public String complemento;
+    public String nome;
     @ManyToOne
-    @JoinColumn(name = "cidade_id")
-    public Cidade cidade_id;
-    public String cep;
-
+    @JoinColumn(name = "estado_id")
+    public Estado estado_id;
 
 }
