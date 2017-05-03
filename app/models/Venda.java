bf3e930556d9fc4fb68d0e87dda78dd6601051c0
William Kolosque <williammaich@hotmail.com>
2017-05-03 11:04:40 -0300

    criado a tabela venda ;

---
+++ app/models/Venda.java
@@ -0,0 +1,29 @@
+package models;
+
+import com.avaje.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.sql.Date;
+
+/**
+ * Created by William on 03/05/2017.
+ */
+@Entity
+public class Venda extends Model{
+
+    @Id
+    public Long id;
+    public Date dataCompra;
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    public Usuario usuario_id;
+    @ManyToOne
+    @JoinColumn(name = "pagamento_id")
+    public Pagamento pagamento_id;
+    public double valorTotal;
+    @ManyToOne
+    public Copa copa_id;
+}
