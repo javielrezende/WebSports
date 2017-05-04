5408c01cc6168919d1906df423ae19f8288ffbcc
Miguel <mgl.deadly@gmail.com>
2017-05-03 23:55:33 -0300

    Removido alguns imports

--- app/models/Carrinho.java
+++ app/models/Carrinho.java
@@ -3,7 +3,6 @@ package models;
 import com.avaje.ebean.Model;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
