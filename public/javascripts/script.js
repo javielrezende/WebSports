e52cef28cd8a79525571c7273d748a4c2acb2f7b
Miguel <mgl.deadly@gmail.com>
2017-05-10 19:14:40 -0300

    Alterado modal para ediçao de eventos do calendario

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,7 +2,7 @@
  * Created by Miguel on 04/05/2017.
  */
 $(document).ready(function() {
-    $('#modelCalendar').hide();
+    $('#modalCalendar').hide();
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -24,10 +24,24 @@ $(document).ready(function() {
         eventLimit: true, // allow "more" link when too many events
         events: '/json',
         eventClick: function (event, jsEvent, view) {
-            $('#modelCalendar').show();
+            $('#modalCalendar').show();
+            $('#title').val(event.title);
+            console.log(moment(event.start, moment.ISO_8601).format());
+            var a = moment.utc(event.start).format(),
+                b = moment.utc(event.end).format();
+            a = a.toString();
+            a = a.substr(0,(a.length - 1));
+            b = b.toString();
+            b = b.substr(0,(b.length - 1));
+
+            $('#start').val(a);
+            $('#end').val(b);
+
         }
 
     });
     $('#calendar').find('.fc-view-container').addClass('col-sm');
-
+    $('#btn-close').on('click', function () {
+       $('#modalCalendar').hide();
+    });
 });
