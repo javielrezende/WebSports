550472b9ae533b6f584df2d2ad608956e80684bc
Williammaich <williammaich@hotmail.com>
2017-07-09 02:17:05 -0300

    criação dos graficos, primeiro comit, criado view, controler

---
+++ app/controllers/GraficoController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by William on 09/07/2017.
+ */
+public class GraficoController extends Controller {
+    public Result index() {
+        return ok(views.html.funcionario.grafico.render());
+    }
+
+    public Result create() {
+        return TODO;
+    }
+
+    public Result save() {
+        return TODO;
+    }
+
+    public Result edit(Integer id) {
+        return TODO;
+    }
+
+    public Result update(Integer id) {
+        return TODO;
+    }
+
+    public Result delete(Integer id) {
+        return TODO;
+    }
+}
